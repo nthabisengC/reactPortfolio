eef83a48c016448615925aa35261a079b2e93f3f
DA MAC M1 74 <damacm174@DA-MACs-MacBook-Air74.local>
2023-09-15 08:42:13 +0200

    Finalise my portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -9,11 +9,15 @@ function About() {
 			<hr />
 			<img className="my-pic" src={process.env.PUBLIC_URL + '/img/Nthabiseng.jpeg'} alt="Nthabiseng"/>
 			<p className="content is-italic mt-4">
-				I'm Nthabiseng, a full stack developer looking to apply herself and contribute the best way she can to any team or project!
+			I'm Nthabiseng, a full stack developer eager to join a team or project and make a valuable contribution!
 			</p>
 			<p className="content">
-				I'm currently a Property Manager that's recently finished the GWU Coding Bootcamp Program for full stack web development.
-				Thank you for taking the time to view my portfolio; please feel free to reach out to me via the contact form above!
+			I have just completed the Digital Academy learnership Program for full stack development. Thank you for checking out my portfolio, and please don't hesitate to contact me using the form above!
+
+
+
+
+
 			</p>
 		</div>
 	);
